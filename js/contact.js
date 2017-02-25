640ce2ef77677c9f8b2488b6b741bcadca194468
Antoine <antoine.normand@hei.fr>
2017-02-25 14:51:00 +0100

    First commit

---
+++ js/contact.js
@@ -0,0 +1,36 @@
+
+var toggled = true;
+
+$("#toggle_menu").click(function(){
+    if(toggled) {
+        toggled=false;
+    $(this).animate({width: "20px"}, "fast");
+    }else {
+        toggled=true;
+            $(this).animate({width: "200px"}, "fast");
+    }
+});
+
+  jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
